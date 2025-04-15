b8b74255ad8700f21613916aff9ff56395a9b643
itstanayhere <tanayaggarwal2@gmail.com>
2025-04-15 14:54:05 +0530

    Added and modified files for deployment

--- app.py
+++ app.py
@@ -136,4 +136,5 @@ def run_all_models(prompt, api_keys):
     })
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
